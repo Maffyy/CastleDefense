68ddd444844fdd239fa9f007ac7c455217513597
Maffyy <martin.studna2@gmail.com>
2018-03-03 22:19:19 +0100

    opengl

--- CastleDefense/Source.cpp
+++ CastleDefense/Source.cpp
@@ -1,6 +1,38 @@
+#include <GL\glut.h>
+#include <GL\GL.h>
 
 
+using namespace std;
 
-int main() {
+void display_callback();
+void reshape_callback();
+
+void init() {
+	glClearColor(1.0,1.0,1.0,1.0);
+}
+
+int main(int argc,char **argv) {
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(1000, 750);
+	glutCreateWindow("CastleDefense");
+	glutDisplayFunc(display_callback);
+	glutReshapeFunc(reshape_callback);
+	init();
+	glutMainLoop();
+	
+	return 0;
+}
+
+void display_callback() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+void reshape_callback(int w,int h) {
+	glViewport(0,0,(GLsizei)w,(GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 
 }
